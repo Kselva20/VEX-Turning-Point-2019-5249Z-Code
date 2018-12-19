ebd974ec71c3a21bf2e0ca37dc0c58ba2df8c3d6
Karthik Selvaraj <31458335+CurryMuncher1127@users.noreply.github.com>
2018-12-19 08:08:14 -0500

    Update robot-config.h

--- PID-Code/TesterClass/robot-config.h
+++ PID-Code/TesterClass/robot-config.h
@@ -12,9 +12,18 @@ vex::motor LeftLift = vex::motor(vex::PORT5,vex::gearSetting::ratio18_1,true);
 
 vex::motor ConveyorBelt = vex::motor(vex::PORT4,vex::gearSetting::ratio18_1,true);
 
-vex::motor FlywheelTop = vex::motor(vex::PORT2,vex::gearSetting::ratio18_1,true);
+vex::motor FlywheelTop = vex::motor(vex::PORT2,vex::gearSetting::ratio18_1,false);
 vex::motor FlywheelBot = vex::motor(vex::PORT1,vex::gearSetting::ratio18_1,false);
 
 vex::timer Timer = vex::timer();
 
 vex::gyro Gyroscope = vex::gyro(Brain.ThreeWirePort.A);
+
+vex::accelerometer AccelX1 = vex::accelerometer(Brain.ThreeWirePort.B);
+vex::accelerometer AccelY1 = vex::accelerometer(Brain.ThreeWirePort.C);
+
+vex::accelerometer AccelX2 = vex::accelerometer(Brain.ThreeWirePort.D);
+vex::accelerometer AccelY2 = vex::accelerometer(Brain.ThreeWirePort.E);
+
+vex::accelerometer AccelX3 = vex::accelerometer(Brain.ThreeWirePort.F);
+vex::accelerometer AccelY3 = vex::accelerometer(Brain.ThreeWirePort.G);
